67e513996c02af7dc324381b64f52e7a9bccf858
samreeve <samphire.reeve4@gmail.com>
2017-09-27 10:21:06 +0000

    Added login functionality and user schema to db

--- app.js
+++ app.js
@@ -2,15 +2,31 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 var Campground = require("./models/campground");
 var seedDB = require("./seeds");
 var Comment = require("./models/comment");
+var User = require("./models/user"); 
 
 
 seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp",{useMongoClient:true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res){
@@ -94,6 +110,27 @@ app.post("/campgrounds/:id/comments", function(req, res){
     });
 });
 
+
+// ======================
+// AUTH ROUTES
+// ======================
+
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The yelp camp server has started");
